94741fa2c286ebefaed2189368e81bdcbca83107
Areen Purohit <areenpurohit@gmail.com>
2024-07-20 16:47:32 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
 let Temperature = 0
 let Light_level = 0
+let Sound_level = 0
 basic.forever(function () {
     Temperature = input.temperature()
     Light_level = input.lightLevel()
+    Sound_level = input.soundLevel()
     basic.showNumber(Temperature)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showNumber(Light_level)
     basic.showLeds(`
         # . . . .
         # . . . .
